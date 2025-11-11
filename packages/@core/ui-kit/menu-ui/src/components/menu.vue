4e3234938d97bac789d6d516c48d4319ef6fe4f6
Konjac-XZ <1951801592@qq.com>
2025-11-11 10:43:24 +0800

    style(menu): increase font weight for menu items to enhance visibility

--- packages/@core/ui-kit/menu-ui/src/components/menu.vue
+++ packages/@core/ui-kit/menu-ui/src/components/menu.vue
@@ -388,6 +388,7 @@ $namespace: vben;
   padding: var(--menu-item-padding-y) var(--menu-item-padding-x);
   margin: 0 var(--menu-item-margin-x) var(--menu-item-margin-y)
     var(--menu-item-margin-x);
+  font-weight: 600;
   font-size: var(--menu-font-size);
   color: var(--menu-item-color);
   white-space: nowrap;
